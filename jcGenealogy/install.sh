86845d78833f99743033743079558dd501c1ea3b
KoalaMuffin <jcu123123@gmail.com>
2018-01-02 17:46:16 -0500

    Update install.sh

--- jcGenealogy/install.sh
+++ jcGenealogy/install.sh
@@ -5,12 +5,11 @@ sudo mkdir /etc/jcGenealogy
 
 read -p "Enter the MySQL username: " dbUser
 read -p "Enter the MySQL password: " dbPassword
-
 mysql -u$dbUser -p$dbPassword -se "CREATE DATABASE jcGenealogy"
-
 sudo echo "\$mysqli = new mysqli(\"127.0.0.1\", \"$dbUser\", \"$dbPassword\", \"jcForum\");" | sudo tee /etc/jcGenealogy/mysqlconf.php
 
-sudo echo "Alias /jcGenealogy /var/lib/jcGenealogy\n<Directory /var/lib/jcGenealogy/>\nOptions +FollowSymLinks\nAllowOverride All\n<IfVersion >= 2.3>\nRequire all granted\n</IfVersion>\n<IfVersion < 2.3>\norder allow,deny\nallow from all\n</IfVersion>\n</Directory>" | sudo tee /etc/apache2/conf-available/jcGenealogy.conf
+read -p "Enter what directory you would like the software to be located at (i.e., if you want it at localhost/familytree, enter 'familytree'): " directory
+sudo echo "Alias /$directory /var/lib/jcGenealogy\n<Directory /var/lib/jcGenealogy/>\nOptions +FollowSymLinks\nAllowOverride All\n<IfVersion >= 2.3>\nRequire all granted\n</IfVersion>\n<IfVersion < 2.3>\norder allow,deny\nallow from all\n</IfVersion>\n</Directory>" | sudo tee /etc/apache2/conf-available/jcGenealogy.conf
 
 sudo wget https://raw.githubusercontent.com/KoalaMuffin/jcGenealogy/master/jcGenealogy/web/register.php -O /var/lib/jcGenealogy/register.php
 
