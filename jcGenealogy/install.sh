a0499969dcae4d7c479ae281643729f46bbff176
KoalaMuffin <jcu123123@gmail.com>
2018-01-02 17:49:14 -0500

    Update install.sh

--- jcGenealogy/install.sh
+++ jcGenealogy/install.sh
@@ -6,7 +6,7 @@ sudo mkdir /etc/jcGenealogy
 read -p "Enter the MySQL username: " dbUser
 read -p "Enter the MySQL password: " dbPassword
 mysql -u$dbUser -p$dbPassword -se "CREATE DATABASE jcGenealogy"
-sudo echo "\$mysqli = new mysqli(\"127.0.0.1\", \"$dbUser\", \"$dbPassword\", \"jcForum\");" | sudo tee /etc/jcGenealogy/mysqlconf.php
+sudo echo "<?php\$mysqli = new mysqli(\"127.0.0.1\", \"$dbUser\", \"$dbPassword\", \"jcForum\");?>" | sudo tee /etc/jcGenealogy/mysqlconf.php
 
 read -p "Enter what directory you would like the software to be located at (i.e., if you want it at localhost/familytree, enter 'familytree'): " directory
 sudo echo "Alias /$directory /var/lib/jcGenealogy\n<Directory /var/lib/jcGenealogy/>\nOptions +FollowSymLinks\nAllowOverride All\n<IfVersion >= 2.3>\nRequire all granted\n</IfVersion>\n<IfVersion < 2.3>\norder allow,deny\nallow from all\n</IfVersion>\n</Directory>" | sudo tee /etc/apache2/conf-available/jcGenealogy.conf
