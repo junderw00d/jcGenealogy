e932b74723f6d30cb8bc3311742576826164d062
KoalaMuffin <jcu123123@gmail.com>
2018-01-02 17:31:33 -0500

    Update install.sh

--- jcGenealogy/install.sh
+++ jcGenealogy/install.sh
@@ -11,4 +11,7 @@ mysql -u$dbUser -p$dbPassword -se "CREATE DATABASE jcGenealogy"
 sudo echo "\$mysqli = new mysqli(\"127.0.0.1\", \"$dbUser\", \"$dbPassword\", \"jcForum\");" | sudo tee /etc/jcGenealogy/mysqlconf.php
 
 sudo echo "Alias /jcGenealogy /var/lib/jcGenealogy\n<Directory /var/lib/jcGenealogy/>\nOptions +FollowSymLinks\nAllowOverride All\n<IfVersion >= 2.3>\nRequire all granted\n</IfVersion>\n<IfVersion < 2.3>\norder allow,deny\nallow from all\n</IfVersion>\n</Directory>" | sudo tee /etc/apache2/conf-available/jcGenealogy.conf
+
+sudo wget https://raw.githubusercontent.com/KoalaMuffin/jcGenealogy/master/jcGenealogy/web/register.php -O /var/lib/jcGenealogy/register.php
+
 sudo echo "<a href='register.php'>register</a>" | sudo tee /var/lib/jcGenealogy/index.html
