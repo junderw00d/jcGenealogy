02a47d552daa10c09296e376b63931adda14ff90
KoalaMuffin <jcu123123@gmail.com>
2018-01-02 18:56:00 -0500

    Create index.php

---
+++ jcGenealogy/web/index.php
@@ -0,0 +1,13 @@
+<?php
+// If you are reading this in your browser, you do not have PHP, which is required for jcGenealogy.
+function generateRandomString($length = 10) {
+  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $charactersLength = strlen($characters);
+  $randomString = '';
+  for ($i = 0; $i < $length; $i++) {
+    $randomString .= $characters[rand(0, $charactersLength - 1)];
+  }
+  return $randomString;
+}
+echo generateRandomString();
+?>
