c3e1f3c3cd2d6ec8f35ba59efd212a709b728c70
KoalaMuffin <jcu123123@gmail.com>
2018-01-02 20:14:31 -0500

    Added apache restart

--- jcGenealogy/install.sh
+++ jcGenealogy/install.sh
@@ -13,6 +13,7 @@ sudo echo -e "<?php\n\$mysqli = new mysqli(\"127.0.0.1\", \"$dbUser\", \"$dbPass
 
 read -p $"Enter what directory you would like the software to be located at (i.e., if you want it at localhost/familytree, enter 'familytree'): " directory
 sudo echo -e "Alias /$directory /var/lib/jcGenealogy\n<Directory /var/lib/jcGenealogy/>\nOptions +FollowSymLinks\nAllowOverride All\n<IfVersion >= 2.3>\nRequire all granted\n</IfVersion>\n<IfVersion < 2.3>\norder allow,deny\nallow from all\n</IfVersion>\n</Directory>" | sudo sudo dd status=none of=/etc/apache2/conf-enabled/jcGenealogy.conf
+sudo service apache2 restart
 
 sudo rm /var/lib/jcGenealogy/* -r
 sudo cp -r web/* /var/lib/jcGenealogy
