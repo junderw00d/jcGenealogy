62626ddee0be13783268cefd57e91fc9bfb069ec
KoalaMuffin <jcu123123@gmail.com>
2018-01-02 19:12:46 -0500

    Added new lines in MySQL config
    Otherwise, it will not work

--- jcGenealogy/install.sh
+++ jcGenealogy/install.sh
@@ -8,7 +8,7 @@ read -p "Enter the MySQL password: " dbPassword
 mysql -u$dbUser -p$dbPassword -se "CREATE DATABASE jcGenealogy"
 echo "Setting up database structure (this may take a while)"
 mysql jcGenealogy -u$dbUser -p$dbPassword < dbstructure.sql
-sudo echo "<?php\$mysqli = new mysqli(\"127.0.0.1\", \"$dbUser\", \"$dbPassword\", \"jcForum\");?>" | sudo tee /etc/jcGenealogy/mysqlconf.php
+sudo echo -e "<?php\n\$mysqli = new mysqli(\"127.0.0.1\", \"$dbUser\", \"$dbPassword\", \"jcForum\");\n?>" | sudo tee /etc/jcGenealogy/mysqlconf.php
 
 read -p $"Enter what directory you would like the software to be located at (i.e., if you want it at localhost/familytree, enter 'familytree'): " directory
 sudo echo -e "Alias /$directory /var/lib/jcGenealogy\n<Directory /var/lib/jcGenealogy/>\nOptions +FollowSymLinks\nAllowOverride All\n<IfVersion >= 2.3>\nRequire all granted\n</IfVersion>\n<IfVersion < 2.3>\norder allow,deny\nallow from all\n</IfVersion>\n</Directory>" | sudo tee /etc/apache2/conf-enabled/jcGenealogy.conf
