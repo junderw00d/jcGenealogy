c95f78b8d7e8b6e3d52077d0fc9cc93d8a87a7e4
KoalaMuffin <jcu123123@gmail.com>
2018-01-04 11:12:38 -0500

    Changes .sh file

--- jcGenealogy/changepassword.sh
+++ jcGenealogy/changepassword.sh
@@ -4,3 +4,4 @@ read -p "Enter the MySQL username: " dbUser
 read -p "Enter the MySQL password: " dbPassword
 
 sudo echo "\$mysqli = new mysqli(\"127.0.0.1\", \"$dbUser\", \"$dbPassword\", \"jcForum\");" | sudo tee /etc/jcGenealogy/mysqlconf.php
+sudo echo -e "dbUser=\"$dbUser\"\ndbPassword=\"$dbPassword\"" | sudo dd status=none of=/etc/jcGenealogy/mysqlconf.sh
