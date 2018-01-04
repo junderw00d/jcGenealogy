e6f66bcff8be463737b7c830d2daa53911bb4fce
KoalaMuffin <jcu123123@gmail.com>
2018-01-03 19:45:33 -0500

    Update install.sh

--- jcGenealogy/install.sh
+++ jcGenealogy/install.sh
@@ -5,14 +5,14 @@ runningDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 sudo mkdir /var/lib/jcGenealogy
 sudo mkdir /etc/jcGenealogy
 
-$mysqlTry = 0
-until mysql -u$dbUser -p$dbPassword -e ";" ; do
-  if [$mysqlTry -eq 1] then
-    echo "Incorrect password."
-  fi
-  read -p "Enter the MySQL username: " dbUser
-  read -p "Enter the MySQL password: " dbPassword
-  $mysqlTry = 1
+function getCredentials {
+  read -r -p "Enter the MySQL username: " dbUser
+  read -r -p -s "Enter the MySQL password: " dbPassword
+}
+getCredentials
+until mysql -u$dbUser -p$dbPassword > /dev/null 2>&1 -e ";" ; do
+  echo "Incorrect username or password. Try again"
+  getCredentials
 done
 
 mysql -u$dbUser -p$dbPassword -se "DROP DATABASE IF EXISTS jcGenealogy"
@@ -23,7 +23,7 @@ mysql jcGenealogy -u$dbUser -p$dbPassword < "$runningDirectory/dbstructure.sql"
 sudo echo -e "<?php\ninclude \"/etc/jcGenealogy/mysqlconf.php\";\n?>" | sudo dd status=none of=/etc/jcGenealogy/load.php
 sudo echo -e "<?php\n\$mysqli = new mysqli(\"127.0.0.1\", \"$dbUser\", \"$dbPassword\", \"jcGenealogy\");\n?>" | sudo dd status=none of=/etc/jcGenealogy/mysqlconf.php
 
-read -p $"Enter what directory you would like the software to be located at (i.e., if you want it at localhost/familytree, enter 'familytree'): " directory
+read -r -p $"Enter what directory you would like the software to be located at (i.e., if you want it at localhost/familytree, enter 'familytree'): " directory
 sudo echo -e "Alias /$directory /var/lib/jcGenealogy/web\n<Directory /var/lib/jcGenealogy/web>\nOptions +FollowSymLinks\nAllowOverride All\n<IfVersion >= 2.3>\nRequire all granted\n</IfVersion>\n<IfVersion < 2.3>\norder allow,deny\nallow from all\n</IfVersion>\n</Directory>" | sudo sudo dd status=none of=/etc/apache2/conf-enabled/jcGenealogy.conf
 sudo service apache2 restart
 
