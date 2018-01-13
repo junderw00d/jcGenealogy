32fc1ff7a6530a182a73838188f660deb10f6b98
KoalaMuffin <jcu123123@gmail.com>
2018-01-13 18:47:13 -0500

    Added newhuman page

--- jcGenealogy/web/newhuman.php
+++ jcGenealogy/web/newhuman.php
@@ -1,4 +1,12 @@
 <?php
 session_start();
 echo"Hello, " . $_SESSION['email'] . "!";
+
+echo "
+<b>Add a human!</b>
+<form method='POST' action='newhuman.php'>
+<input type='checkbox' id='me' name='me'>
+<label for='me'>This is ME!</label>
+</form>
+";
 ?>
