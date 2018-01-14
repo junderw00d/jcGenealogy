cbc7be2de313ab819cae5e528d0939b591daea98
KoalaMuffin <jcu123123@gmail.com>
2018-01-13 20:30:38 -0500

    Update newhuman.php

--- jcGenealogy/web/newhuman.php
+++ jcGenealogy/web/newhuman.php
@@ -5,6 +5,11 @@ echo"Hello, " . $_SESSION['email'] . "!";
 echo "
 <b>Add a human!</b>
 <form method='POST' action='newhuman.php'>
+<p><b>Full name: </b><input name='name'></p>
+
+<input type='checkbox' id='alive' name='alive'>
+<label for='alive'>Alive</label>
+
 <input type='checkbox' id='me' name='me'>
 <label for='me'>This is ME!</label>
 </form>
