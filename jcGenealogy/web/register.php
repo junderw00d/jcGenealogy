31a4b67dc58685131d3bde40b08755551156dc61
KoalaMuffin <jcu123123@gmail.com>
2018-01-13 21:26:01 -0500

    fixed salt, again.

--- jcGenealogy/web/register.php
+++ jcGenealogy/web/register.php
@@ -31,7 +31,7 @@ if ($_POST['register'] != true) {
                 	       
 			session_start();
 			$_SESSION['email'] = $_POST['email'];
-			$_SESSION['password'] = hash("sha512", $_POST['password'] . $salt);
+			$_SESSION['password'] = hash("sha512", $_POST['password']);
 			
 			echo"Now, you can <a href='newhuman.php'>add your first human</a>!";
 		} else {
