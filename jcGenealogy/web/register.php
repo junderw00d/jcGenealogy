f6322c73053cae0659d2dbc6c4f2acff0ebd86eb
KoalaMuffin <jcu123123@gmail.com>
2018-01-13 20:48:13 -0500

    Update register.php

--- jcGenealogy/web/register.php
+++ jcGenealogy/web/register.php
@@ -24,13 +24,16 @@ if ($_POST['register'] != true) {
 		
 
 		if ($accessCheck->fetch_object()->original == 1) {
-                        session_start();
-			$_SESSION['email'] = $_POST['email'];
 			echo "Welcome to jcGenealogy.";
 			$mysqli->query("TRUNCATE TABLE users");
 			$salt = hash("sha512", uniqid(mt_rand(), true));
 			$mysqli->query("INSERT INTO users (email, password, salt) VALUES ('" . $_POST['email'] . "', '" . hash("sha512", $_POST['password'] . $salt) . "', '" . $salt . "')");
-                	echo"Now, you can <a href='newhuman.php'>add your first human</a>!";
+                	       
+			session_start();
+			$_SESSION['email'] = $_POST['email'];
+			$_SESSION['password'] = hash("sha512", $_POST['password'] . $salt);
+			
+			echo"Now, you can <a href='newhuman.php'>add your first human</a>!";
 		} else {
                 	echo "<form id='selectHumanForm' method='POST' action='register.php'>";
                 	echo "<select name='humanLink' id='select' form='selectHumanForm'>";
