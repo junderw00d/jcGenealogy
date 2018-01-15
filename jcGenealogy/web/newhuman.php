d872887bb69452552910cff888cc02b1024752be
KoalaMuffin <jcu123123@gmail.com>
2018-01-15 18:27:13 -0500

    Update newhuman.php

--- jcGenealogy/web/newhuman.php
+++ jcGenealogy/web/newhuman.php
@@ -15,16 +15,21 @@ if ($loggedin === true) {
                                         <input type='checkbox' id='me' name='me'>
                                         <label for='me'>This is ME!</label>
                                 </p>
-                                <p><b>Date of death: </b><input type='date' name='dead' id='dead'></p>
+                                <p><b>Date of death: </b><input type='date' name='deaddate' id='dead'></p>
                                 <input type='hidden' name='form' value='newhuman'>
-                                <input type='hidden' name='deadhidden' id='deadhidden'>
+                                
                                 <input type='submit'>
                         </form>
 			<script src='newhuman.js'></script>
                         ";
         } else {
                 $nameArray = explode(" ", $_POST['name']);
-                $mysqli->query("INSERT INTO humans (firstname, lastname, alive, deathdate) VALUES ('" . $nameArray[0] . "', '" . $nameArray[sizeof($nameArray) - 1]. "', '" . $_POST['alive'] . "', '" . $_POST['dead'] . "')");
+		if ($_POST['alive'] == "on") {
+			$alive = 1;	
+		} else {
+			$alive = 0;	
+		}
+                $mysqli->query("INSERT INTO humans (firstname, lastname, alive, deathdate) VALUES ('" . $nameArray[0] . "', '" . $nameArray[sizeof($nameArray) - 1]. "', '" . $alive . "', '" . $_POST['deaddate'] . "')");
         }
 } else {
         echo "Not logged in!!!";
