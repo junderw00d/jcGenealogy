e9fd95ee1f8adca298d1708f881592523883c6a5
KoalaMuffin <jcu123123@gmail.com>
2018-01-14 21:05:53 -0500

    Update install.sh

--- jcGenealogy/install.sh
+++ jcGenealogy/install.sh
@@ -5,6 +5,8 @@ runningDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 sudo mkdir /var/lib/jcGenealogy
 sudo mkdir /etc/jcGenealogy
 
+sudo mv "$runningDirectory/checkuser.php" /etc/jcGenealogy/
+
 function getCredentials {
   read -r -p "Enter the MySQL username: " dbUser
   read -s -r -p "Enter the MySQL password: " dbPassword
@@ -22,7 +24,6 @@ mysql jcGenealogy -u$dbUser -p$dbPassword < "$runningDirectory/dbstructure.sql"
 
 sudo echo -e "<?php\ninclude \"/etc/jcGenealogy/mysqlconf.php\";\ninclude \"/etc/jcGenealogy/checkuser.php\";\n?>" | sudo dd status=none of=/etc/jcGenealogy/load.php
 sudo echo -e "<?php\n\$mysqli = new mysqli(\"127.0.0.1\", \"$dbUser\", \"$dbPassword\", \"jcGenealogy\");\n?>" | sudo dd status=none of=/etc/jcGenealogy/mysqlconf.php
-sudo echo -e "<?php\nsession_start();\nfunction checkCreds() {\n\$userCheck = \$mysqli->query(\"SELECT password, salt WHERE email='\" . \$_SESSION['email'] . \"'\")->fetch_assoc();\nif (hash(\"sha512\", \$_SESSION['password'] . \$salt) == \$userCheck['password'] ) {\nreturn true;\n}\n}\n?>" | sudo dd status=none of=/etc/jcGenealogy/checkuser.php
 
 sudo echo -e "dbUser=\"$dbUser\"\ndbPassword=\"$dbPassword\"" | sudo dd status=none of=/etc/jcGenealogy/mysqlconf.sh
 
