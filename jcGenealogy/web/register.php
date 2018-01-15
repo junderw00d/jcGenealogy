358c7f8b3ff3d099352529ac4106b3345cdab837
KoalaMuffin <jcu123123@gmail.com>
2018-01-14 21:04:02 -0500

    Update register.php

--- jcGenealogy/web/register.php
+++ jcGenealogy/web/register.php
@@ -5,13 +5,13 @@ if ($_POST['register'] != true) {
                 <p><b>Email: </b><input type='email' name='email' required></p>
                 <p><b>Password: </b><input type='password' name='password' required></p>
                 <p><b>Confirm Password: </b><input type='password' name='confirmpassword' required></p>";
-		if ($_POST['accesscode'] === null) {
-                	echo "<p><b>Access Code: </b><input name='accesscode' required></p>";
-		} else {
-			echo "<input type='hidden' name='accesscode' value='" . $_POST['accesscode'] . "'>";
-		}
+                if ($_POST['accesscode'] === null) {
+                        echo "<p><b>Access Code: </b><input name='accesscode' required></p>";
+                } else {
+                        echo "<input type='hidden' name='accesscode' value='" . $_POST['accesscode'] . "'>";
+                }
                 echo"
-		<input type='hidden' name='register' value='true'>
+                <input type='hidden' name='register' value='true'>
                 <input type='submit'>
         </form>
         ";
@@ -21,34 +21,34 @@ if ($_POST['register'] != true) {
         if ($accessCheck->num_rows === 0) {
                 echo "No such access code, or the access code is expired.";
         } else {
-		
+                
 
-		if ($accessCheck->fetch_object()->original == 1) {
-			echo "Welcome to jcGenealogy.";
-			$mysqli->query("TRUNCATE TABLE users");
-			$salt = hash("sha512", uniqid(mt_rand(), true));
-			$mysqli->query("INSERT INTO users (email, password, salt) VALUES ('" . $_POST['email'] . "', '" . hash("sha512", $_POST['password'] . $salt) . "', '" . $salt . "')");
-                	       
-			session_start();
-			$_SESSION['email'] = $_POST['email'];
-			$_SESSION['password'] = $_POST['password'];
-			
-			echo"Now, you can <a href='newhuman.php'>add your first human</a>!";
-		} else {
-                	echo "<form id='selectHumanForm' method='POST' action='register.php'>";
-                	echo "<select name='humanLink' id='select' form='selectHumanForm'>";
-                	$humanGen = $mysqli->query("SELECT * FROM humans ORDER BY lastname");
-                	while ($humanGenResult = $humanGen->fetch_assoc()) {
-                	        echo "<option"; 
-                	        if ($humanGenResult['alive'] == 0) {
-                	        	echo " disabled";
-                	        }
+                if ($accessCheck->fetch_object()->original == 1) {
+                        echo "Welcome to jcGenealogy.";
+                        $mysqli->query("TRUNCATE TABLE users");
+                        $salt = hash("sha512", uniqid(mt_rand(), true));
+                        $mysqli->query("INSERT INTO users (email, password, salt) VALUES ('" . $_POST['email'] . "', '" . hash("sha512", hash("sha512",$_POST['password']) . $salt) . "', '" . $salt . "')");
+                               
+                        session_start();
+                        $_SESSION['email'] = $_POST['email'];
+                        $_SESSION['password'] = hash("sha512", $_POST['password']);
+                        
+                        echo"Now, you can <a href='newhuman.php'>add your first human</a>!";
+                } else {
+                        echo "<form id='selectHumanForm' method='POST' action='register.php'>";
+                        echo "<select name='humanLink' id='select' form='selectHumanForm'>";
+                        $humanGen = $mysqli->query("SELECT * FROM humans ORDER BY lastname");
+                        while ($humanGenResult = $humanGen->fetch_assoc()) {
+                                echo "<option"; 
+                                if ($humanGenResult['alive'] == 0) {
+                                        echo " disabled";
+                                }
                         echo" value='" . $humanGenResult['id'] . "'>" . $humanGenResult['firstname'] . " " . $humanGenResult['lastname'] . "</option>";
-                	}
-               		echo "</select><br>";
-                	echo "<input type='submit'></form>";
-                	echo "<button onclick='idwt'>I do not want to link my account to a member of the family tree.</button>";
-		}
+                        }
+                        echo "</select><br>";
+                        echo "<input type='submit'></form>";
+                        echo "<button onclick='idwt'>I do not want to link my account to a member of the family tree.</button>";
+                }
         }
 }
 ?>
