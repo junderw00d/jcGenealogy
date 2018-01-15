6f7a242c40301c03b3c447cbe925469a33717e9c
KoalaMuffin <jcu123123@gmail.com>
2018-01-15 10:25:47 -0500

    Update newhuman.php

--- jcGenealogy/web/newhuman.php
+++ jcGenealogy/web/newhuman.php
@@ -4,26 +4,42 @@ if ($loggedin === true) {
         if ($_POST['form'] !== "newhuman") {
                 echo"Hello, " . $_SESSION['email'] . "!";
                 echo "
-			<b>Add a human!</b>
-			<form method='POST' action='newhuman.php'>
-				<p><b>Full name: </b><input name='name'></p>
-				<p>
-					<input type='checkbox' id='alive' name='alive'>
-					<label for='alive'>Alive</label>
-				</p>
-				<p>
-					<input type='checkbox' id='me' name='me'>
-					<label for='me'>This is ME!</label>
-				</p>
-				<input type='hidden' name='form' value='newhuman'>
-				<input type='submit'>
-			</form>
-        		";
-	} else {
-		$nameArray = explode(" ", $_POST['name']);
-		echo "It looks like you want to add a human with... name1=" . $nameArray[0] . "and two=" . $nameArray[sizeof($nameArray) - 1];
-		
-	}
+                        <b>Add a human!</b>
+                        <form method='POST' action='newhuman.php'>
+                                <p><b>Full name: </b><input name='name'></p>
+                                <p>
+                                        <input type='checkbox' id='alive' name='alive'>
+                                        <label for='alive'>Alive</label>
+                                </p>
+                                <p>
+                                        <input type='checkbox' id='me' name='me'>
+                                        <label for='me'>This is ME!</label>
+                                </p>
+                                <input type='hidden' name='form' value='newhuman'>
+                                <input type='submit'>
+                        </form>
+                        <script>
+                                originalAlive = false;
+
+                                alive = document.getElementById('alive')
+                                me = document.getElementById('me');
+                                alive.onclick = function() {
+                                        originalAlive = alive.checked;
+                                }
+                                me.onclick = function() {
+                                        if (me.checked === true) {
+                                                alive.checked = true;
+                                        } else {
+                                                alive.checked = originalAlive;
+                                        }
+                                }
+                        </script>
+                        ";
+        } else {
+                $nameArray = explode(" ", $_POST['name']);
+                echo "It looks like you want to add a human with... name1=" . $nameArray[0] . "and two=" . $nameArray[sizeof($nameArray) - 1];
+                
+        }
 } else {
         echo "Not logged in!!!";
 }
