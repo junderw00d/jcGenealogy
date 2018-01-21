1ab815d25fa59f13431ba14d8d9263486c5e040c
KoalaMuffin <jcu123123@gmail.com>
2018-01-21 10:00:58 -0500

    Update login.php

--- jcGenealogy/web/login.php
+++ jcGenealogy/web/login.php
@@ -1,25 +1,26 @@
 <?php
 
 if ($_POST['login'] === null) {
-	echo "
-		<form action='login.php' method='POST'>
-			<p><b>Email: </b><input type='email' name='email'></p>
-			<p><b>Password: <input type='password name='password'></p>
-			<input type='hidden' name='login' value='1'>
-			<input type='submit' value='Log in'>
-		</form>
- 	";
+        echo "
+                <form action='login.php' method='POST'>
+                        <p><b>Email: </b><input type='email' name='email'></p>
+                        <p><b>Password: <input type='password' name='password'></p>
+                        <input type='hidden' name='login' value='1'>
+                        <input type='submit' value='Log in'>
+                </form>
+        ";
 } else {
-	
-	session_start();
-        
-	$_SESSION['email'] = $_POST['email'];
-	$_SESSION['password'] = hash("sha512", $_POST['password']);
-	
-	include "/etc/jcGenealogy/load.php";
-	
-	echo $loggedin;
-	
+        session_start();
+        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['password'] = hash("sha512", $_POST['password']);
+        include "/etc/jcGenealogy/load.php";
+        if ($loggedin == true) {
+                $_SESSION['id'] = $mysqli->query("SELECT id FROM users WHERE email ='" . $_POST['email'] . "'")->fetch_assoc()['id'];
+                echo "You're logged in now.";
+        } else {
+                echo "nonononononono";
+                session_destroy();
+        }
 }
 
 ?>
