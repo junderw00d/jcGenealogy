92db1260f84ea0376c71af19d4ae7bb6f5c57f3b
KoalaMuffin <jcu123123@gmail.com>
2018-01-22 18:34:34 -0500

    Update human.php

--- jcGenealogy/web/human.php
+++ jcGenealogy/web/human.php
@@ -7,11 +7,17 @@ body {
         padding-left:10%;
         padding-right:10%;
 }
+h1 {
+//      float: left;
+}
+
 table {
+        position:sticky;
         border-radius: 8px;
         border:solid;
         float:right;
         padding:5px;
+        margin-top:20px;
 }
 th {
         text-align:left;
@@ -19,6 +25,9 @@ th {
 #fullname {
         font-size:125%;
         text-align:center;
+}
+hr {
+border-color: black;
 }
 </style>
 </head>
@@ -34,24 +43,36 @@ if ($humanQuery['gender'] == 1) {
         $gender = "Female";
 }
 
-echo "
+$birthDateDate = date_create($humanQuery['birthdate']);
+$currentDate = date_create(date("Y-m-d"));
+$age = floor(date_diff($birthDateDate, $currentDate)->format("%a") / 365);
 
+$months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+$birthdateExplode = explode("-", $humanQuery['birthdate']);
+
+
+$birthday = $months[intval(ltrim($birthdateExplode[1]), 0) - 1] . " " . ltrim($birthdateExplode[2],0) . ", " . $birthdateExplode[0];
+
+echo "
+<div id='content'>
+<h1>$fullname</h1>
+<hr>
 <table>
         <tbody>
-                <tr><td id='fullname' colspan='2'><span>" . $fullname . "</span></td></tr>
+                <tr id='top'><td id='fullname' colspan='2'><span>" . $fullname . "</span></td></tr>
                 <tr>
                         <th scope='row'>Gender</th>
                         <td>" . $gender .  "</td>
                 </tr>
                 <tr>
                         <th scope='row'>Date of birth</th>
-                        <td>" . $humanQuery['birthdate'] . "</td>
+                        <td>" . $birthday . " (age $age)</td>
                 </tr>
 
         </tbody>
 </table>
 
-
+</div>
 "
 
 ?>
