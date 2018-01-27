7b3b76c5462d9c1b7d725bf985e51ab262308166
KoalaMuffin <jcu123123@gmail.com>
2018-01-27 17:48:13 -0500

    Update human.php

--- jcGenealogy/web/human.php
+++ jcGenealogy/web/human.php
@@ -2,22 +2,24 @@
 <head>
 <style>
 body {
+        background:#F8F9F9;
         padding-top:1%;
 
         padding-left:10%;
         padding-right:10%;
 }
 h1 {
-//      float: left;
+        vertical-align: middle;
+        display:inline;
 }
 
-table {
+#table {
         position:sticky;
         border-radius: 8px;
         border:solid;
         float:right;
         padding:5px;
-        margin-top:20px;
+        margin-left:10px;
 }
 th {
         text-align:left;
@@ -27,7 +29,26 @@ th {
         text-align:center;
 }
 hr {
-border-color: black;
+        border-color: black;
+}
+#biography {
+        display:inline;
+}
+#summary {
+        outline:none;
+}
+#summary:hover {
+        cursor:pointer;
+}
+#page-content {
+        padding:20px;
+        background:white;
+        border-radius:10px;
+}
+#edit {
+        display:inline;
+        float:right;
+        vertical-align: middle;
 }
 </style>
 </head>
@@ -54,26 +75,56 @@ $birthdateExplode = explode("-", $humanQuery['birthdate']);
 $birthday = $months[intval(ltrim($birthdateExplode[1]), 0) - 1] . " " . ltrim($birthdateExplode[2],0) . ", " . $birthdateExplode[0];
 
 echo "
-<div id='content'>
+<div id='page-content'>
+<div id='human-header'>
 <h1>$fullname</h1>
+<div id='edit'><img src='assets/edit.svg' width='15px'> <a href='humanedit.php'>Edit</a></div>
+<br>
 <hr>
-<table>
-        <tbody>
-                <tr id='top'><td id='fullname' colspan='2'><span>" . $fullname . "</span></td></tr>
-                <tr>
-                        <th scope='row'>Gender</th>
-                        <td>" . $gender .  "</td>
-                </tr>
-                <tr>
-                        <th scope='row'>Date of birth</th>
-                        <td>" . $birthday . " (age $age)</td>
-                </tr>
+</div>
 
-        </tbody>
-</table>
+<div id='table'>
+        <details open ontoggle='detailsToggle()' id='details'>
+                <summary id='summary'><small id='quickfactstoggle'>Hide Quick Facts</small></summary>
+                <table>
+                        <tbody>
+                                <tr id='top'><td id='fullname' colspan='2'><span>" . $fullname . "</span></td></tr>
+                                <tr>
+                                        <th scope='row'>Gender</th>
+                                        <td>" . $gender .  "</td>
+                                </tr>
+                                <tr>
+                                        <th scope='row'>Date of birth</th>
+                                        <td>" . $birthday . " (age $age)</td>
+                                </tr>
+                        </tbody>
+                </table>
+        </details>
+</div>
+
+<div id='biography'>
+        <p>
+aoisdjgoijsdfoigj soidjfg iosdjfg iosjdfgi osjdfgipo jsdifpgo jsdofipg jsidofpg 
+sidufg idfhgu isdhfgui sdhfgui hdsfuigh dsfuigh dfisugh disufgh disufgh dfisughs
+sdifugh siudfhgui sfdhgiu sdfhgiu sdguis dfguisdfhgui sdhfgui hdsfuigh usidfhgui shdfg 
+sdiufgh uisdfhgui sdhfgui hdsuifghs idfhgiu sdhfgui sdhfiug hdisufghisu dfhgui sdhfuig
+suidfhg isudfhgiu sdhfgui sdhfugi dhsiughsi iusdfhgiu sdhfgiu hsdif uhsduf h g
+        </p>
+</div>
 
 </div>
 "
 
 ?>
+
+<script>
+function detailsToggle() {
+        if (document.getElementById("details").open === true) {
+                document.getElementById("quickfactstoggle").innerHTML = "Hide Quick Facts";
+        } else {
+                document.getElementById("quickfactstoggle").innerHTML = "Show Quick Facts";
+        }
+}
+</script>
+
 </body>
